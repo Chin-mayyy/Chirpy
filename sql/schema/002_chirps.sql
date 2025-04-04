71f8fcd0af05050ab6aa47a89db2cc9bd2c17c54
Chinmay Anand <chinmay27anand@gmail.com>
2025-04-04 21:19:09 +0530

    Authorization completed!

--- sql/schema/002_chirps.sql
+++ sql/schema/002_chirps.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 CREATE TABLE chirps (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-    created_at TIMESTAMP DEFAULT NOW (),
-    updated_at TIMESTAMP DEFAULT NOW (),
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     body TEXT NOT NULL,
     user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
 
 -- +goose Down
+DROP TABLE chirps;
